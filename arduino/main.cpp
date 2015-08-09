5da997cf52d789ee7667b76f8b99e0d4dceae595
Charles Rymal <chuck1@users.noreply.github.com>
2015-08-09 11:17:04 -0700

    Update main.cpp

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -1,7 +1,8 @@
 
 
-Accel_Gyro_Data agd;
+Accel_Gyro ag;
 
+Flight_Control fc;
 
 void setup()
 {
@@ -9,9 +10,17 @@ void setup()
 
 void loop()
 {
+  // read and store ag data
+  ag.read();
   
-
-
-
-
+  // user input
+  ui.read();
+  
+  // calculate new motor speeds based on accelgyro and user input
+  fc.calc(ag, ui);
+  // and write to speed controllers
+  fc.write();
+  
+  // camera
+  cam.read_and_send();
 }
